dc02b8f9c4eca7f0840b426c94fad9bf4d05efd8
Leonid Semenov <ln.sm.study@gmail.com>
2022-12-14 20:59:51 +0600

    Initial Commit

---
+++ assistant/assistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  assistantApp.swift
+//  assistant
+//
+//  Created by Leonid Semenov on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct assistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
